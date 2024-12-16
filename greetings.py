e158aaf4bedd4f2ac4f52d70fc8674916c6adc8a
ZeroDragon12 <klimi2004@gmail.com>
2024-12-17 01:32:03 +0500

    new file

---
+++ greetings.py
@@ -0,0 +1,12 @@
+username = 'user'
+title = 'title'
+content = 'content'
+status = 'not completed'
+created_date = "10-11-2024"
+issue_date = "11-12-2024"
+print('Имя пользователя:',username)
+print('Название:',title)
+print('Описание:',content)
+print('Статус:',status)
+print('Дата создания:',created_date)
+print('Дедлайн:',issue_date)
